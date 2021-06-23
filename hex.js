1f376d6669b9a476f909d2746ef9568d74e0dac7
dev-from-cbd <veryfatwombat@gmail.com>
2021-06-24 07:40:57 +1000

    js

--- hex.js
+++ hex.js
@@ -5,7 +5,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   let hexColor = "#";
   for (let i = 0; i < 6; i++) {
-    hexColor += hex[0];
+    hexColor += hex[getRandomNumber()];
   }
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
